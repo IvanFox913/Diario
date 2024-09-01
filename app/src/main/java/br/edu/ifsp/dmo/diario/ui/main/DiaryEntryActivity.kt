4035bd0d54e771ac0e761f3598b7fb8cb5804c97
IvanFox913 <ivanfox913@hotmail.com>
2024-08-31 21:28:28 -0300

    Adição do Layout da Activity de salvar/editar DiaryEntry
    Meu Android Studio ocasionalmente está simplesmente ignorando as regras de constraints. Fechar e abrir resolve. Alquimia...

---
+++ app/src/main/java/br/edu/ifsp/dmo/diario/ui/main/DiaryEntryActivity.kt
@@ -0,0 +1,21 @@
+package br.edu.ifsp.dmo.diario.ui.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.dmo.diario.R
+
+class DiaryEntryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_diary_entry)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
